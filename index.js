d153cd3b5dd388ff3435a0c9f5a326129f2f4981
Aris Fresta <frestaris@gmail.com>
2024-05-16 12:52:12 +1000

    added reset btn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const player1 = document.querySelector(".img1");
 const player2 = document.querySelector(".img2");
 const result = document.querySelector("h1");
+const resetBtn = document.querySelector(".reset-btn");
 
 function randomNumber() {
   return Math.floor(Math.random() * 6) + 1;
@@ -9,6 +10,13 @@ function randomNumber() {
 function rollDice() {
   rollDice1();
   rollDice2();
+  if (player1.src > player2.src) {
+    result.textContent = "🚩 P.1 wins!";
+  } else if (player1.src < player2.src) {
+    result.textContent = "🚩 P.2 wins!";
+  } else {
+    result.textContent = "Draw!";
+  }
 }
 function rollDice1() {
   let random1 = randomNumber();
@@ -23,10 +31,4 @@ function rollDice2() {
 
 rollDice();
 
-if (player1.src > player2.src) {
-  result.textContent = "🚩 P.1 wins!";
-} else if (player1.src < player2.src) {
-  result.textContent = "🚩 P.2 wins!";
-} else {
-  result.textContent = "Draw!";
-}
+resetBtn.addEventListener("click", rollDice);
